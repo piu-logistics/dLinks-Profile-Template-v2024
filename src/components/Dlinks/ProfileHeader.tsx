b0c8216eba2dbc9bf15571f3fc86d613695dc442
“Ibtisam <ibtisam.arif@casculate.com>
2023-09-20 18:00:47 +0200

    feat: add opacity icon.

--- src/components/Dlinks/ProfileHeader.tsx
+++ src/components/Dlinks/ProfileHeader.tsx
@@ -5,6 +5,7 @@ import {
   Grid,
   GridItem,
   HStack,
+  Icon,
   Text,
   VStack,
 } from "@chakra-ui/react";
@@ -13,7 +14,18 @@ import FullScreenImage from "../Global/FullScreenImage";
 import user from "@/assets/user.png";
 import passport from "@/assets/passport.jpg";
 import animation from "@/assets/animation.mp4";
-const ProfileHeaderSmall = () => {
+import { FaPlus, FaShareAlt } from "react-icons/fa";
+import { useState } from "react";
+
+interface ProfileHeaderProps {
+  opacity: number;
+  increaseOpacityByStep: () => void;
+}
+
+const ProfileHeaderSmall = ({
+  increaseOpacityByStep,
+  opacity,
+}: ProfileHeaderProps) => {
   return (
     <Box
       display={["block", "block", "block", "none"]}
@@ -35,11 +47,36 @@ const ProfileHeaderSmall = () => {
           <source src={animation} type="video/mp4" />
         </video>
       </AspectRatio>
+      <Box
+        position={"absolute"}
+        top={0}
+        objectFit={"cover"}
+        left={0}
+        w={"100%"}
+        h={"full"}
+        zIndex={-1}
+        opacity={opacity}
+        bg={"black"}
+      />
+      <Icon
+        as={FaPlus}
+        onClick={increaseOpacityByStep}
+        fill={"white"}
+        cursor={"pointer"}
+        borderRadius={"md"}
+        h={6}
+        w={6}
+        shadow={"md"}
+        mt={2}
+        _hover={{ bg: "white", fill: "black" }}
+        ml={2}
+      />
       <Grid
         templateColumns={["1fr"]}
         pt={[2, 4, 10]}
         px={[2, 10, 20]}
         zIndex={2}
+        userSelect={"none"}
       >
         <GridItem
           display={"flex"}
@@ -57,9 +94,10 @@ const ProfileHeaderSmall = () => {
             maxW={"fit-content"}
             mt={2}
             fontSize={"xs"}
-            h={6}
-            w={4}
             px={2}
+            h={"auto"}
+            py={1}
+            leftIcon={<Icon as={FaShareAlt} />}
           >
             Share
           </Button>
@@ -78,7 +116,7 @@ const ProfileHeaderSmall = () => {
           <Text color={"white"}>
             Passport Ref:{" "}
             <Text as={"b"} color={"white"}>
-              1st gen 400
+              1st Gen 30
             </Text>
           </Text>
           <Text color={"white"}>
@@ -88,13 +126,16 @@ const ProfileHeaderSmall = () => {
             </Text>
           </Text>
 
-          <Text color={"white"}>User Name</Text>
+          <Text color={"white"}> Citizen: AlemAkuchi</Text>
         </GridItem>
       </Grid>
     </Box>
   );
 };
-const ProfileHeaderLarge = () => {
+const ProfileHeaderLarge = ({
+  opacity,
+  increaseOpacityByStep,
+}: ProfileHeaderProps) => {
   return (
     <Box
       display={["none", "none", "none", "block"]}
@@ -116,16 +157,54 @@ const ProfileHeaderLarge = () => {
           <source src={animation} type="video/mp4" />
         </video>
       </AspectRatio>
+      <Box
+        position={"absolute"}
+        top={0}
+        objectFit={"cover"}
+        left={0}
+        w={"100%"}
+        h={"full"}
+        zIndex={-1}
+        opacity={opacity}
+        bg={"black"}
+      />
+      <Icon
+        as={FaPlus}
+        onClick={increaseOpacityByStep}
+        fill={"white"}
+        cursor={"pointer"}
+        borderRadius={"md"}
+        h={6}
+        w={6}
+        shadow={"md"}
+        mt={2}
+        ml={2}
+        _hover={{ bg: "white", fill: "black" }}
+      />
       <Grid
         templateColumns={["1fr 1fr 1fr"]}
         pt={[2, 4, 10]}
         px={[2, 10, 20]}
         zIndex={2}
         pb={4}
+        userSelect={"none"}
       >
-        <GridItem display={"flex"} flexDirection={"column"} p={20} gap={2}>
+        <GridItem
+          display={"flex"}
+          flexDirection={"column"}
+          p={20}
+          gap={2}
+          alignItems={"center"}
+        >
           <FullScreenImage imageUrl={Dlinks} />
-          <Button>Share</Button>
+          <Button
+            maxW={"fit-content"}
+            mt={2}
+            px={2}
+            leftIcon={<Icon as={FaShareAlt} />}
+          >
+            Share
+          </Button>
         </GridItem>
         <GridItem display={"flex"} flexDirection={"column"} px={[0, 5, 5, 10]}>
           <FullScreenImage
@@ -138,7 +217,7 @@ const ProfileHeaderLarge = () => {
           <Text color={"white"}>
             Passport Ref:{" "}
             <Text as={"b"} color={"white"}>
-              1st gen 400
+              1st Gen 30
             </Text>
           </Text>
           <Text color={"white"}>
@@ -154,7 +233,7 @@ const ProfileHeaderLarge = () => {
             containerStyles={{ border: "2px solid white" }}
           />
           <Text placeSelf={"end"} color={"white"}>
-            User Name
+            Citizen: AlemAkuchi
           </Text>
         </GridItem>
       </Grid>
@@ -163,10 +242,29 @@ const ProfileHeaderLarge = () => {
 };
 
 const ProfileHeader = () => {
+  const [opacity, setOpacity] = useState(1);
+
+  const increaseOpacityByStep = () => {
+    //increase opacity by step of 0.1 with mod
+    console.log(opacity);
+    if (opacity < 1) {
+      setOpacity(opacity + 0.1);
+    }
+
+    if (opacity >= 1) {
+      setOpacity(0.1);
+    }
+  };
   return (
     <>
-      <ProfileHeaderSmall />
-      <ProfileHeaderLarge />
+      <ProfileHeaderSmall
+        increaseOpacityByStep={increaseOpacityByStep}
+        opacity={opacity}
+      />
+      <ProfileHeaderLarge
+        increaseOpacityByStep={increaseOpacityByStep}
+        opacity={opacity}
+      />
     </>
   );
 };
