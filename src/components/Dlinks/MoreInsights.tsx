e8e95faae8e931c32a2ea8aa82a025e582647396
“Ibtisam <ibtisam.arif@casculate.com>
2023-11-03 10:58:22 +0100

    remaining changes

--- src/components/Dlinks/MoreInsights.tsx
+++ src/components/Dlinks/MoreInsights.tsx
@@ -1,57 +1,242 @@
 import { Box, Flex, Heading, Image, Text, VStack } from "@chakra-ui/react";
 import banner from "@/assets/images/banner.jpg";
-const insights = [
-  {
-    title: "Level Being 9 Apiary",
-    description: (
-      <Text>
-        Level Being 9 Apiary is interested in all things to do with the STEM and
-        Art of beekeeping. We are developing a directory and networking forum of
-        beekeeping organisation and practitioners panAfrica.
-      </Text>
-    ),
-  },
-  {
-    title: "Community Science Lab",
-    description: (
-      <Text>
-        uLearn Naturally Community Science Lab seeks to establish community
-        based learning centres focused on STEM, explored in culturally inclusive
-        and engaging ways. Community Science Labs will focus on; a) Communal
-        science lab facilities - made accessible to all, b) Space observatory -
-        telescopes and radio, c) STEM Open Learning Resource rooms, d)
-        Planetarium - VR and immersive projections, e) Lab cafe - light
-        edutainment spaces.
-      </Text>
-    ),
-  },
-  {
-    title: "Creators’ Hubs",
-    description: (
-      <Text>
-        We will be developing a localised learners cooperative hubs around STEM
-        and the first pilot project “DIY Build A Recumbent Trike project”. This
-        project will involve learners in design (CAD), metalworking (frame
-        building), general recumbent trike construction to point of completion,
-        quality testing and naturally, in cycling. The main intention being
-        about engaging children, youth and adults in transferable STEM
-        competency development in the context of environmental awareness and the
-        advancement of healthful (more sustainable) ways of travelling.
-      </Text>
-    ),
-  },
-  {
-    title: "DopA - Directory of panAfrica",
-    description: (
-      <Text>
-        We are developing a directory and networking forum of panAfrican
-        Organisations and Coordinators.
-      </Text>
-    ),
-  },
-];
+import { useContext } from "react";
+import { profileContext } from "../../context/ProfileContext";
 
 const MoreInsights = () => {
+  const { profile } = useContext(profileContext);
+  const {
+    dopAbasicallyIAAm,
+    dopAorgAddressCity,
+    dopAorgAddressCountry,
+    dopAorgAddressPostalCode,
+    dopAorgAddressState,
+    dopAorgAddressStreet,
+    dopAorgName,
+    dopApositionRoleinOrg,
+    dopAsomeWordsAboutMyInterest,
+    lB9AbasicallyIAm,
+    lB9AorgAddressCity,
+    lB9AorgAddressCountry,
+    lB9AorgAddressPostalCode,
+    lB9AorgAddressState,
+    lB9AorgAddressStreet,
+    lB9AorgName,
+    lB9ApositionRoleinOrg,
+    lB9AsomeWordsAboutMyInterest,
+    cSLbasicallyIAm,
+    cSLorgAddressCity,
+    cSLorgAddressCountry,
+    cSLorgAddressPostalCode,
+    cSLorgAddressState,
+    cSLorgAddressStreet,
+    cSLorgName,
+    cSLpositionRoleinOrg,
+    cSLsectorsAndIndustries,
+    cSLsomeWordsAboutMyInterest,
+    cHsectorsAndIndustries,
+    cHsomeWordsAboutMyInterest,
+    cHbasicallyIAm,
+    cHorgAddressCity,
+    cHorgAddressCountry,
+    cHorgAddressPostalCode,
+    cHorgAddressState,
+    cHorgAddressStreet,
+    cHorgName,
+    cHpositionRoleinOrg,
+  } = profile;
+
+  const insights = [
+    {
+      title: "Level Being 9 Apiary",
+      description: (
+        <>
+          <Text>
+            Level Being 9 Apiary is interested in all things to do with the STEM
+            and Art of beekeeping. We are developing a directory and networking
+            forum of beekeeping organisation and practitioners panAfrica.
+          </Text>
+          <Box>
+            {lB9AbasicallyIAm && (
+              <Text>
+                I'm adding this Level Being 9 Apiary dLinks Profile Enhancement
+                because I am&nbsp;
+                <Text as={"span"} fontWeight={"bold"}>
+                  {lB9AbasicallyIAm}
+                </Text>
+              </Text>
+            )}
+            {lB9ApositionRoleinOrg && (
+              <Text>
+                Position / Role in Organisation {lB9ApositionRoleinOrg}
+              </Text>
+            )}
+            {lB9AorgName && <Text>Organisation Name {lB9AorgName}</Text>}
+            {lB9AorgAddressStreet && (
+              <Text> Street {lB9AorgAddressStreet}</Text>
+            )}
+            {lB9AorgAddressCity && <Text> City {lB9AorgAddressCity}</Text>}
+            {lB9AorgAddressState && <Text> State {lB9AorgAddressState}</Text>}
+            {lB9AorgAddressCountry && (
+              <Text> Country {lB9AorgAddressCountry}</Text>
+            )}
+            {lB9AorgAddressPostalCode && (
+              <Text> Postal Code {lB9AorgAddressPostalCode}</Text>
+            )}
+            {lB9AsomeWordsAboutMyInterest && (
+              <Text> About My Interests {lB9AsomeWordsAboutMyInterest}</Text>
+            )}
+          </Box>
+        </>
+      ),
+    },
+    {
+      title: "Community Science Lab",
+      description: (
+        <>
+          <Text>
+            uLearn Naturally Community Science Lab seeks to establish community
+            based learning centres focused on STEM, explored in culturally
+            inclusive and engaging ways. Community Science Labs will focus on;
+            a) Communal science lab facilities - made accessible to all, b)
+            Space observatory - telescopes and radio, c) STEM Open Learning
+            Resource rooms, d) Planetarium - VR and immersive projections, e)
+            Lab cafe - light edutainment spaces.
+          </Text>
+          <Box display={"flex"} flexDirection={"column"} gap={4}>
+            {cSLbasicallyIAm && (
+              <Text>
+                I'm adding this Community Science Labs dLinks Profile
+                Enhancement because I am &nbsp;
+                <Text as={"span"} fontWeight={"bold"}>
+                  {cSLbasicallyIAm}
+                </Text>
+              </Text>
+            )}
+            {cSLpositionRoleinOrg && (
+              <Text>
+                Position / Role in Organisation {cSLpositionRoleinOrg}
+              </Text>
+            )}
+            {cSLorgName && <Text>Organisation Name {cSLorgName}</Text>}
+            {cSLorgAddressStreet && <Text> Street {cSLorgAddressStreet}</Text>}
+            {cSLorgAddressCity && <Text> City {cSLorgAddressCity}</Text>}
+            {cSLorgAddressState && <Text> State {cSLorgAddressState}</Text>}
+            {cSLorgAddressCountry && (
+              <Text> Country {cSLorgAddressCountry}</Text>
+            )}
+            {cSLorgAddressPostalCode && (
+              <Text> Postal Code {cSLorgAddressPostalCode}</Text>
+            )}
+
+            {cSLsomeWordsAboutMyInterest && (
+              <Text> About My Interests {cSLsomeWordsAboutMyInterest}</Text>
+            )}
+            {cSLsectorsAndIndustries && (
+              <Text>
+                {" "}
+                Sectors and industries of special interest to me.{" "}
+                {cSLsectorsAndIndustries}
+              </Text>
+            )}
+          </Box>
+        </>
+      ),
+    },
+    {
+      title: "Creators’ Hubs",
+      description: (
+        <>
+          <Text>
+            We will be developing a localised learners cooperative hubs around
+            STEM and the first pilot project “DIY Build A Recumbent Trike
+            project”. This project will involve learners in design (CAD),
+            metalworking (frame building), general recumbent trike construction
+            to point of completion, quality testing and naturally, in cycling.
+            The main intention being about engaging children, youth and adults
+            in transferable STEM competency development in the context of
+            environmental awareness and the advancement of healthful (more
+            sustainable) ways of travelling.
+          </Text>
+          <Box display={"flex"} flexDirection={"column"} gap={4}>
+            {cHbasicallyIAm && (
+              <Text>
+                I'm adding this Creators' Hubs dLinks Profile Enhancement
+                because I am {""}
+                <Text as={"span"} fontWeight={"bold"}>
+                  {cHbasicallyIAm}
+                </Text>
+              </Text>
+            )}
+
+            {cHpositionRoleinOrg && (
+              <Text>Position / Role in Organisation {cHpositionRoleinOrg}</Text>
+            )}
+            {cHorgName && <Text>Organisation Name {cHorgName}</Text>}
+            {cHorgAddressStreet && <Text> Street {cHorgAddressStreet}</Text>}
+            {cHorgAddressCity && <Text> City {cHorgAddressCity}</Text>}
+            {cHorgAddressState && <Text> State {cHorgAddressState}</Text>}
+            {cHorgAddressCountry && <Text> Country {cHorgAddressCountry}</Text>}
+            {cHorgAddressPostalCode && (
+              <Text> Postal Code {cHorgAddressPostalCode}</Text>
+            )}
+
+            {cHsomeWordsAboutMyInterest && (
+              <Text> About My Interests {cHsomeWordsAboutMyInterest}</Text>
+            )}
+            {cHsectorsAndIndustries && (
+              <Text>
+                Sectors and industries of special interest to me.{" "}
+                {cSLsectorsAndIndustries}
+              </Text>
+            )}
+          </Box>
+        </>
+      ),
+    },
+    {
+      title: "DopA - Directory of panAfrica",
+      description: (
+        <>
+          <Text>
+            We are developing a directory and networking forum of panAfrican
+            Organisations and Coordinators.
+          </Text>
+          <Box display={"flex"} flexDirection={"column"} gap={4}>
+            {dopAbasicallyIAAm && (
+              <Text>
+                I'm adding this DopA dLinks Profile Enhancement because I am{" "}
+                <Text as={"span"} fontWeight={"bold"}>
+                  {dopAbasicallyIAAm}
+                </Text>
+              </Text>
+            )}
+            {dopApositionRoleinOrg && (
+              <Text>
+                Position / Role in Organisation {dopApositionRoleinOrg}
+              </Text>
+            )}
+            {dopAorgName && <Text>Organisation Name {dopAorgName}</Text>}
+            {dopAorgAddressStreet && (
+              <Text> Street {dopAorgAddressStreet}</Text>
+            )}
+            {dopAorgAddressCity && <Text> City {dopAorgAddressCity}</Text>}
+            {dopAorgAddressState && <Text> State {dopAorgAddressState}</Text>}
+            {dopAorgAddressCountry && (
+              <Text> Country {dopAorgAddressCountry}</Text>
+            )}
+            {dopAorgAddressPostalCode && (
+              <Text> Postal Code {dopAorgAddressPostalCode}</Text>
+            )}
+            {dopAsomeWordsAboutMyInterest && (
+              <Text> About My Interests {dopAsomeWordsAboutMyInterest}</Text>
+            )}
+          </Box>
+        </>
+      ),
+    },
+  ];
+
   return (
     <Flex flexDirection={"column"} gap={8}>
       <Heading color={"black"}>OnWeb3 Radio</Heading>
--- src/components/Dlinks/sections/InterestAndCulture/Web6Functions.tsx
+++ src/components/Dlinks/sections/InterestAndCulture/Web6Functions.tsx
@@ -4,11 +4,9 @@ import {
   Heading,
   Image,
   ListItem,
-  Text,
   UnorderedList,
   VStack,
 } from "@chakra-ui/react";
-import gem from "@/assets/Quartz.png";
 import { useContext } from "react";
 import { profileContext } from "../../../../context/ProfileContext";
 
@@ -30,9 +28,13 @@ const Web6Functions = () => {
           <Heading py={10} fontSize={["1rem", "1.25rem"]}>
             Precious Material Choice:
           </Heading>
-          <Text>{preciousMaterialChoice[0]}</Text>
+
+          {preciousMaterialChoice[0]}
         </Box>
-        <Image src={gem} h="120px" w={"120px"} />
+        <Image
+          blendMode={"multiply"}
+          src={`https://alkebulanmeta.app/wp-content/uploads/2023/11/Gem-Material-${preciousMaterialChoice[0]}-100x100.png.jpg`}
+        />
       </Flex>
     </VStack>
   );
